a0b39776a17a7f9eb8e2e5dc8cdda7854e607e6e
icaiow <101759288+icaiow@users.noreply.github.com>
2023-06-04 22:03:34 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+
+
+import ProductList from './src/ProductLista';
+import ProductItem from "./src/ProductItem";
+
+const HomeScreen = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <ProductList />
+  
+    </View>
+  );
+  
+
+
+
+
+
+
+
+
+  
+  
+  
+
+  
+
+  
+
+};
+
+export default HomeScreen;
